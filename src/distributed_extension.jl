965b7a3927d4ea0abf6aba0a6c08ebb6f93a420b
Daniel Alcalde <d.alcalde.puente@fz-juelich.de>
2023-07-17 10:54:10 +0200

    everywhere_async

---
+++ src/distributed_extension.jl
@@ -0,0 +1,36 @@
+function remotecall_eval_async(m::Module, procs, ex)
+    
+    run_locally = 0
+    for pid in procs
+        if pid == myid()
+            run_locally += 1
+        else
+            Distributed.@async_unwrap remotecall_wait(Core.eval, pid, m, ex)
+        end
+    end
+    yield() # ensure that the remotecalls have had a chance to start
+
+    # execute locally last as we do not want local execution to block serialization
+    # of the request to remote nodes.
+    for _ in 1:run_locally
+        @async Core.eval(m, ex)
+    end
+
+    return nothing
+end
+
+macro everywhere_async(ex)
+    procs = GlobalRef(@__MODULE__, :procs)
+    return esc(:($(SRMPS).@everywhere_async $procs() $ex))
+end
+
+macro everywhere_async(procs, ex)
+    imps = Distributed.extract_imports(ex)
+    return quote
+        $(isempty(imps) ? nothing : Expr(:toplevel, imps...)) # run imports locally first
+        let ex = Expr(:toplevel, :(task_local_storage()[:SOURCE_PATH] = $(get(task_local_storage(), :SOURCE_PATH, nothing))), $(esc(Expr(:quote, ex)))),
+            procs = $(esc(procs))
+            remotecall_eval_async(Main, procs, ex)
+        end
+    end
+end
