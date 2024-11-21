add13319bbdeb8fb9c0d36262d2c1676a57568a1
Daniel Alcalde <d.alcalde.puente@fz-juelich.de>
2024-11-21 13:00:43 +0100

    improve performance of get_precomp_sOψ_elems

--- src/GenericEksAndOks/Eks.jl
+++ src/GenericEksAndOks/Eks.jl
@@ -12,7 +12,7 @@ Base.ndims(t::TensorOperatorSum) = ndims(t.hilbert)
     TensorOperatorSum(tensors, hilbert, sites)
     Generates a TensorOperatorSum from a hamiltonian and a hilbert space. It precomputes the sites where the operator acts on.
 """
-function TensorOperatorSum(ham::OpSum, hilbert::Array)
+function TensorOperatorSum(ham::OpSum, hilbert::Array; combine_tensors=true)
     
     tensors = Vector{ITensor}(undef, length(ham))
     sites = Vector{Vector{Int}}(undef, length(ham))
@@ -27,23 +27,15 @@ function TensorOperatorSum(ham::OpSum, hilbert::Array)
         tensors[i] = ITensor(o, hilbert[:])
         sites[i] = get_active_sites(o)
     end
-    TensorOperatorSum(tensors, hilbert, sites)
+    tso = TensorOperatorSum(tensors, hilbert, sites)
+    if combine_tensors
+        tso = combine_tensors_at_same_site(tso)
+    end
+    return tso
 end
 
-"""
-Generates a Dictionary with the entries being (sample_config=> energy_weight) so that Ek=sum energy_weight * exp(logψ(sample_config) - logψ(sample))
-get_precomp_sOψ_elems(
-    tensor::ITensor, 
-    sites::Vector, 
-    sample_, 
-    hilbert; 
-    sum_precompute = DefaultOrderedDict(()->0), 
-    offset=1, 
-    get_flip_sites=false # If true, it will give a Dictionary Dict(sites_flipped=>energy_weight) instead of Dict(s'=>energy_weight)
-    sites_fliiped: First index is the site, second the new s'_i.
-)
-"""
-function get_precomp_sOψ_elems!(tensor::ITensor, sites::Vector, sample_, hilbert; sum_precompute=DefaultOrderedDict(()->0), offset=1, get_flip_sites=false)
+
+function get_precomp_sOψ_elems_slow!(tensor::ITensor, sites::Vector, sample_, hilbert; sum_precompute=DefaultOrderedDict(()->0), offset=1, get_flip_sites=false)
     sample_r = sample_[sites]
     hilbert_r = hilbert[sites]
     
@@ -66,17 +58,49 @@ function get_precomp_sOψ_elems!(tensor::ITensor, sites::Vector, sample_, hilber
         end
     end
 
-    # Make real if the imaginary part is too small
-    for (key, value) in sum_precompute
-        if value isa Complex && abs(imag(value))/(abs(real(value))+1e-10) < 1e-14
-            sum_precompute[key] = real(value)
-        end
+    return sum_precompute
+end
 
-        if sum_precompute[key] == 0.
-            delete!(sum_precompute, key)
+"""
+Generates a Dictionary with the entries being (sample_config => energy_weight) so that Ek=sum energy_weight * exp(logψ(sample_config) - logψ(sample))
+get_precomp_sOψ_elems(
+    tensor::ITensor, 
+    sites::Vector, 
+    sample_, 
+    hilbert; 
+    sum_precompute = DefaultOrderedDict(()->0), 
+    offset=1, 
+    get_flip_sites=false # If true, it will give a Dictionary Dict(sites_flipped=>energy_weight) instead of Dict(s'=>energy_weight)
+    sites_fliiped: First index is the site, second the new s'_i.
+)
+"""
+function get_precomp_sOψ_elems!(tensor::ITensor, sites::Vector, sample_, hilbert; sum_precompute=DefaultOrderedDict(()->0), offset=1, get_flip_sites=false, fast=true)
+    
+    sample_r = sample_[sites]
+    hilbert_r = hilbert[sites]
+    
+    indices_sample = collect(hi' => s for (hi, s) in zip(hilbert_r, sample_r)) # Selects the indices that act on the tensor from the left O|s>
+    
+    tensor_proj = onehot(indices_sample) * tensor
+    
+    # Make sure that the indices have the right permutation
+    perm = NDTensors.getperm(ITensors.inds(tensor_proj), hilbert_r)
+    tensor_proj = ITensor(permutedims(tensor_proj.tensor, perm))
+    
+    inds = findall(x-> x != 0, tensor_proj.tensor)
+    for ind in inds
+        sample_r2 = ind.I
+        vi = tensor_proj[ind]
+        if get_flip_sites
+            key = find_flip_site(sample_r .- offset, sample_r2 .- offset, sites)
+        else
+            sample_M = deepcopy(sample_)
+            sample_M[sites] .= sample_r2
+            key = sample_M .- offset
         end
-    end 
-
+        sum_precompute[key] += vi
+    end
+    
     return sum_precompute
 end
 
@@ -112,13 +136,23 @@ function get_precomp_sOψ_elems(tso::Vector{TensorOperatorSum}, sample_::Array; 
 end
 
 
-function get_precomp_sOψ_elems(tso::TensorOperatorSum, sample_::Array{T, N}; sum_precompute=DefaultOrderedDict(()->0), offset=1, get_flip_sites=false) where {T <: Int, N}
+function get_precomp_sOψ_elems(tso::TensorOperatorSum, sample_::Array{T, N}; sum_precompute=DefaultOrderedDict(()->0), offset=1, get_flip_sites=false, kwargs...) where {T <: Int, N}
     @assert size(sample_) == size(tso)
     sample_ = sample_[:] # Flatten the sample
     sample_ = sample_ .+ offset # Shift the sample from 0...N-1 to 1...N
     @assert all(sample_ .> 0) "Sample must be composed of positive integers instead of $sample_"
+    
     for (tensor, sites) in zip(tso.tensors, tso.sites)
-        get_precomp_sOψ_elems!(tensor, sites, sample_, tso.hilbert[:]; sum_precompute, offset, get_flip_sites)
+        get_precomp_sOψ_elems!(tensor, sites, sample_, tso.hilbert[:]; sum_precompute, offset, get_flip_sites, kwargs...)
+    end
+    
+    # Remove zeros and make real if the imaginary part is too small
+    for (key, value) in sum_precompute
+        if value == 0.
+            delete!(sum_precompute, key)
+        elseif value isa Complex && abs(imag(value))/(abs(real(value))+1e-10) < 1e-14
+            sum_precompute[key] = real(value)
+        end
     end
 
     # If not 1D, reshape the samples
@@ -127,6 +161,7 @@ function get_precomp_sOψ_elems(tso::TensorOperatorSum, sample_::Array{T, N}; su
     end
     return sum_precompute
 end
+
 function inner_div_ψ(sample_, tso::TensorOperatorSum, logψ_func; logψ_sample=nothing)
     sum_precompute = get_precomp_sOψ_elems(tso, sample_)
 
@@ -224,4 +259,20 @@ function reduce_dim(O::ITensors.OpSum, size::Tuple)
         reduce_dim!(oi, size)
     end
     return O_c
+end
+
+second(x) = x[2]
+function combine_tensors_at_same_site(tso::TensorOperatorSum)
+    d = Dict()
+    for (sites, tensor) in zip(tso.sites, tso.tensors)
+        if sites in keys(d)
+            d[sites] .+= tensor 
+        else
+            d[sites] = tensor         
+        end
+    end
+    tensors_sites = collect(d)
+    sites = first.(tensors_sites)
+    tensors = second.(tensors_sites)
+    return TensorOperatorSum(tensors, tso.hilbert, sites)
 end
