f53317422503eae071b4004bf91dce4350193051
Daniel Alcalde <d.alcalde.puente@fz-juelich.de>
2025-03-28 23:05:28 +0100

    refactor outlier detection functions and introduce bootstrap method for improved robustness

--- src/NaturalGradient/outlier.jl
+++ src/NaturalGradient/outlier.jl
@@ -1,7 +1,7 @@
 function find_outliers(Eks; cut=1.0)
     order = sortperm(Eks)
     s_Eks = Eks[order]
-    magnitude = mean(abs.(s_Eks[2:end-1]))
+    #magnitude = mean(abs.(s_Eks[2:end-1]))
     magnitude = mean(abs.(s_Eks))
 
     cutter = diff(Eks[order]) ./ magnitude
@@ -11,14 +11,33 @@ function find_outliers(Eks; cut=1.0)
     return sort(vcat(order[1:cut_small], order[cut_big:end]))
 end
 
-function remove_outliers!(Eks::AbstractVector, args...; importance_weights=nothing, cut=1.0, verbose=false)
-    local remove
-    if importance_weights !== nothing
-        remove = find_outliers(Eks .* importance_weights; cut)
-    else
-        remove = find_outliers(Eks; cut)
-    end
+function bootrap_energy(Eks; importance_weights=ones(length(Eks)))
+    Eks2 = Eks .^ 2
+    wm = sum(importance_weights)
+    Em = sum(Eks .* importance_weights)
+    Ev = sum(Eks2 .* importance_weights)
+    
+    E_boot = (Em .- Eks .* importance_weights) ./ (wm .- importance_weights)
+    Ev_boot = (Ev .- Eks2 .* importance_weights) ./ (wm .- importance_weights)
+    return E_boot, Ev_boot .- E_boot.^2
+end
 
+
+function find_outliers_bootstrap(Eks; cut=0.1, importance_weights=ones(length(Eks)))
+    E_boot, Ev_boot = bootrap_energy(Eks; importance_weights)
+    error1 = abs.(Ev_boot .- mean(Ev_boot)) ./ abs.(Ev_boot)
+    error2 = abs.(E_boot .- mean(E_boot)) ./ abs.(E_boot)
+    error = max.(error1, error2)
+    return findall(error .> cut)
+end
+
+"""
+    remove_outliers!(Eks, args...; importance_weights=nothing, cut=1.0, verbose=false)
+    find outliers in Eks and remove them from Eks and all other args.
+"""
+function remove_outliers!(Eks::AbstractVector, args...; importance_weights=ones(length(Eks)), cut=0.005, verbose=false, find_outliers_=find_outliers_bootstrap)
+    remove = find_outliers_(Eks; cut, importance_weights)
+    
     local args_new
     if length(remove) > 0
         if verbose
@@ -35,6 +54,8 @@ function remove_outliers!(Eks::AbstractVector, args...; importance_weights=nothi
     end
     return args_new
 end
+
+remove_outliers(Eks::AbstractVector, args...; kwargs...) = remove_outliers!(copy(Eks), deepcopy(args)...; kwargs...)
 
 function deleteat_(m::AbstractMatrix, v::Vector{<:Integer})
     new_o = setdiff(1:size(m, 1), v)
